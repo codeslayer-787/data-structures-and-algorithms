48a5716c9bc77547754d547d5b09cf9b8c04738c
codeslayer-787 <irwin_ayala@yahoo.com>
2021-02-21 17:06:23 -0600

    Adds solution to problem 4

--- code-challenges/challenges-05.test.js
+++ code-challenges/challenges-05.test.js
@@ -81,6 +81,9 @@ For example, if the input is 'Welcome', the output will be:
 const howMuchPencil = (str) => {
   let result = [];
   // Solution code here...
+    for (let i = 0; i <= str.length; i++){
+      result.push(str.slice(i))
+    }
   return result;
 };
 
@@ -280,7 +283,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return a list of shortening words', () => {
     expect(howMuchPencil('Welcome')).toStrictEqual(['Welcome', 'elcome', 'lcome', 'come', 'ome', 'me', 'e', '']);
     expect(howMuchPencil('Welcome').length).toStrictEqual(8);
