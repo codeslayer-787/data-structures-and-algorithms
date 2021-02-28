b88748b8322726eae07770028bd0bc415138e9a6
codeslayer-787 <irwin_ayala@yahoo.com>
2021-02-28 13:11:58 -0600

    Solves 1-7

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -81,6 +81,9 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map(function (num){
+    return 2**num;
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -96,7 +99,7 @@ For example: charCode(['h','i']) returns [104, 105].
 const charCode = (arr) => {
   // Solution code here...
   const output = arr.map((value) => {
-  return value.charCode(0);
+  return value.charCodeAt(0);
 });
 return output;
 };
@@ -258,7 +261,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(mapTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(mapTwoToThe([0, 4, 5]).length).toStrictEqual(3);
@@ -269,7 +272,7 @@ xdescribe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
