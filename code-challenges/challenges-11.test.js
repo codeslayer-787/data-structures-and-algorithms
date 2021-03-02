65f7bc712778546b7f96e7848f607dab031457fa
codeslayer-787 <irwin_ayala@yahoo.com>
2021-03-02 10:54:28 -0600

    Minor fixes

--- code-challenges/challenges-11.test.js
+++ code-challenges/challenges-11.test.js
@@ -41,6 +41,7 @@ If the word is between 5 and 10 characters long, return true. Otherwise, return 
 
 const validateWord = (word) => {
   // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
